e6ab8a08c990cdd53839e1fd09203156ea7481bb
Andy Borsz <andy.borsz@gmail.com>
2012-05-03 23:04:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneJasmine::Application.config.secret_token = 'fd952614f4edd4ffee19d54f5d2e237411560d011eade15c0b9f3b1a1192511d2a162b98b79d11834782bf916315779af79ec06f12080c2300c0be2e4813b8f1'
